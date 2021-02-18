f65650f7d8790fdb89dc7f7a174eed0e92605a67
Akshay <gaikwadakshay663@gmail.com>
2021-02-18 11:47:44 +0530

    Added Calculator Operations

---
+++ Calculator.js
@@ -0,0 +1,12 @@
+import Addition from "./Addition";
+import Subtraction from "./Subtraction";
+import Multiplication from "./Multiplication";
+import Division from  "./Division";
+
+const x=10;
+const y=20;
+
+console.log(Addition(x, y))
+console.log(Subtraction(x, y))
+console.log(Multiplication(x, y))
+console.log(Division(x, y))
